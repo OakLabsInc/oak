4783d4709051b4bc3e7978fe74823519fe0c6958
Flynn Joffray <nucleardreamer@gmail.com>
2017-04-27 09:13:00 -0700

    bump version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-OAK_VERSION="2.0.0-rc1"
+OAK_VERSION="2.0.0"
 BASE="oaklabs/oak:$OAK_VERSION"
 
 # our FROM line in the Dockerfile, should ideally match the current electron node version
