57fad90e25a8c3d74e0c96dd611822b15bd63060
Flynn Joffray <nucleardreamer@gmail.com>
2017-05-23 12:05:18 -0700

    manifest in build file

--- build.sh
+++ build.sh
@@ -1,20 +1,33 @@
 #!/bin/bash -e
 
-OAK_VERSION="2.1.2"
-BASE="oaklabs/oak:$OAK_VERSION"
+OAK_VERSION="2.1.2";
+BASE="oaklabs/oak:$OAK_VERSION";
 
 # our FROM line in the Dockerfile, should ideally match the current electron node version
-FROM="node"
-NODE_VERSION="7.4.0"
-FROM_TAG="$NODE_VERSION-slim"
+FROM="node";
+NODE_VERSION="7.4.0";
+FROM_TAG="$NODE_VERSION-slim";
 
-NPM_URL="https://registry.npmjs.org/"
+NPM_URL="https://registry.npmjs.org/";
 
-# these will get tagged, by default, as oaklabs/oak:VER-ARCH-nvidia-XXX.XX
-NVIDIA_VERSIONS=( "367.44" "375.26" "375.39" "378.13" )
+DOCKERFILE_TEMPLATE_PATH="./node_modules/.bin/dockerfile-template";
+MANIFEST_TOOL_PATH="/usr/bin/manifest-tool";
 
-DOCKERFILE_TEMPLATE_PATH="./node_modules/.bin/dockerfile-template"
-UNAME_ARCH=$(uname -m)
+UNAME_ARCH=$(uname -m);
+
+if [[ $# -lt 3 && $1 == "manifest" ]]; then
+    if [ ! -e "$DOCKERFILE_TEMPLATE_PATH" ]; then
+        echo "";
+        echo "* manifest-tool missing. Install it so you can use it!.";
+        echo "";
+        exit 1;
+    else
+        $MANIFEST_TOOL_PATH push from-spec manifest.yml
+        echo "";
+        echo "* manifest pushed successfully.";
+        exit 0;
+    fi
+fi
 
 if [ ! -e "$DOCKERFILE_TEMPLATE_PATH" ]; then
   echo "";
@@ -61,20 +74,8 @@ echo "** Building $FULL_TAG";
 echo "";
 docker build -t $FULL_TAG $(pwd);
 
-# ./build.sh nvidia will build nvidia version tags
-if [[ $# -lt 3 && $1 == "nvidia" ]]; then
-    for VERSION in "${NVIDIA_VERSIONS[@]}"; do
-        TAG="${FULL_TAG}-nvidia${VERSION}";
-        echo "";
-        echo "** Building $TAG";
-        echo "";
-        docker build --build-arg NVIDIA=true --build-arg NVIDIA_VERSION=$VERSION -t $TAG $(pwd);
-        TAGS+=("${TAG}");
-    done;
-fi
-
 # push our tags array
-if [[ $# -lt 3 && ($1 == "push" || $2 == "push") ]]; then
+if [[ $# -lt 3 && $1 == "push" ]]; then
     for TAG in "${TAGS[@]}"; do
         echo "";
         echo "** Pushing $TAG";
