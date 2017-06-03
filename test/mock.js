283669caa3f2652cdfeee12a4a05e7a3099d2213
Flynn Joffray <nucleardreamer@gmail.com>
2017-06-03 15:35:26 -0700

    basic tests

--- test/mock.js
+++
@@ -1,8 +0,0 @@
-process.env.NODE_ENV = 'test'
-const { EventEmitter2 } = require('eventemitter2')
-
-module.exports.message = new EventEmitter2({
-  wildcard: true,
-  newListener: false,
-  maxListeners: 200
-})
