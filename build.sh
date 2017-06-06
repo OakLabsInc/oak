5a8d775946823326c90546715404efa2e174a32c
Flynn Joffray <nucleardreamer@gmail.com>
2017-06-06 13:50:54 -0700

    modules and scripts are separate to renderer

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-OAK_VERSION="2.1.2";
+OAK_VERSION="2.2.0";
 BASE="oaklabs/oak:$OAK_VERSION";
 
 # our FROM line in the Dockerfile, should ideally match the current electron node version
