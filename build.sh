f999a4a2cb862a57f14772cf74901e1f73609efc
Flynn Joffray <nucleardreamer@gmail.com>
2017-06-26 13:52:59 -0700

    3.1.0, standard formatting

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-OAK_VERSION="3.1.0-rc2";
+OAK_VERSION="3.1.0";
 BASE="oaklabs/oak:$OAK_VERSION";
 
 # our FROM line in the Dockerfile, should ideally match the current electron node version
 FROM="node";
-NODE_VERSION="7.9.0";
+NODE_VERSION="7.4.0";
 FROM_TAG="$NODE_VERSION-slim";
 
 NPM_URL="https://registry.npmjs.org/";
