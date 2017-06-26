17fc5f8776a18d1769017367f3dfc7bfe035b3b5
Flynn Joffray <nucleardreamer@gmail.com>
2017-06-26 02:43:29 -0700

    3.1.0-rc1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-OAK_VERSION="3.0.1";
+OAK_VERSION="3.1.0-rc1";
 BASE="oaklabs/oak:$OAK_VERSION";
 
 # our FROM line in the Dockerfile, should ideally match the current electron node version
