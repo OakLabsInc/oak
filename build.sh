026bf5a4e5ee00c5f022c3ab253e2f002df6c46b
Flynn Joffray <nucleardreamer@gmail.com>
2017-06-29 13:27:08 -0400

    removing ONBUILD frm dockerfile

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-OAK_VERSION="3.1.2";
+OAK_VERSION="3.1.3";
 BASE="oaklabs/oak:$OAK_VERSION";
 
 # our FROM line in the Dockerfile, should ideally match the current electron node version
