8904b21d1a172fa51f4cf94cf5156cd92265f8a8
Flynn Joffray <nucleardreamer@gmail.com>
2017-12-19 15:27:17 -0800

    fixing multi window example

---
+++ examples/multiple-windows/src/index.js
@@ -0,0 +1,67 @@
+const { join } = require('path')
+const oak = require('oak')
+
+const Hapi = require('hapi')
+const inert = require('inert')
+
+// load hapi, serve static files
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: join(__dirname)
+      }
+    }
+  }
+})
+
+server.connection({
+  port: process.env.PORT || 9999
+})
+
+server.register(inert, () => {})
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      index: true
+    }
+  }
+})
+
+// start hapi
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+})
+oak.catchErrors()
+// main app
+oak.on('ready', () => {
+  let one = oak.load({
+    url: 'http://localhost:9999/index.html',
+    scripts: ['lodash'],
+    fullscreen: false,
+    ontop: false
+  }).on('switch', switchWindowFocus)
+
+  let two = oak.load({
+    url: 'http://localhost:9999/index.html',
+    scripts: ['lodash'],
+    fullscreen: false,
+    ontop: false
+  }).on('switch', switchWindowFocus)
+
+  function switchWindowFocus () {
+    oak.log.info('switching focus')
+    if (one.isFocused()) {
+      two.focus()
+    } else {
+      one.focus()
+    }
+  }
+})
