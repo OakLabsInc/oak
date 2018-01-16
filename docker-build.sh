9c4a277f12898e3dc1e6184abae6162bc732a983
Flynn Joffray <nucleardreamer@gmail.com>
2018-01-16 15:54:25 -0500

    build script fix

--- docker-build.sh
+++ docker-build.sh
@@ -16,20 +16,6 @@ MANIFEST_TOOL_PATH=$(which manifest-tool);
 
 UNAME_ARCH=$(uname -m);
 
-if [[ $# -lt 3 && $1 == "manifest" ]]; then
-    if [ ! -e "$MANIFEST_TOOL_PATH" ]; then
-        echo "";
-        echo "* manifest-tool missing. Install it so you can use it!.";
-        echo "";
-        exit 1;
-    else
-        $MANIFEST_TOOL_PATH push from-spec manifest.yml
-        echo "";
-        echo "* manifest pushed successfully.";
-        exit 0;
-    fi
-fi
-
 # The immediate targets are just for intel and arm (raspi)
 case $UNAME_ARCH in
   x86_64)
@@ -61,8 +47,6 @@ $DOCKERFILE_TEMPLATE_PATH \
     -d FROM_TAG=$FROM_TAG \
     -d ELECTRON_VERSION=$ELECTRON_VERSION > Dockerfile
 
-npm uninstall dockerfile-template
-
 # build our base tag
 echo "";
 echo "** Building $FULL_TAG";
@@ -76,5 +60,16 @@ if [[ $# -lt 3 && $1 == "push" ]]; then
         echo "** Pushing $TAG";
         echo "";
         docker push $TAG;
+        if [ ! -e "$MANIFEST_TOOL_PATH" ]; then
+            echo "";
+            echo "* manifest-tool missing. Install it so you can use it!.";
+            echo "";
+            exit 1;
+        else
+            $MANIFEST_TOOL_PATH push from-spec manifest.yml
+            echo "";
+            echo "* manifest pushed successfully.";
+            exit 0;
+        fi
     done;
 fi
