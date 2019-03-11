aef79fe484790dfd73b2f19f883c260f0ec40414
zeroice <nucleardreamer@gmail.com>
2019-03-11 15:27:09 -0700

    updating multiplewindows example

--- examples/multiple-windows/README.md
+++ examples/multiple-windows/README.md
@@ -7,26 +7,30 @@ This is an example of spawning multiple windows within one app, and sending mess
 The main server starts, serving up two windows with `oak.load`, pointed at `index.html`
 
 ```
-$ npm start ─────┐
-$ docker-compose up ─┐
-                      │
-                      │┌ `let one = oak.load(...)`─ src/index.html
-         src/index.js └┤
-                        └ `let two = oak.load(...)`─ src/index.html
+        $ npm start ┐
+$ docker-compose up ┐
+                    │
+                    │┌> `let one = oak.load(...)`─> src/one.html
+       src/index.js └┤
+                      └> `let two = oak.load(...)`─> src/two.html
 ```
 
 ## Messages
 
-Each window sends an event from `index.html`, to eventually fire an event in the other window.
+Each window sends an event from the `html` page, to eventually fire an event in the other window.
 
 ```
-* window one - index.html
-  oak.send('switch') ────┐
-                             │ * window two - index.js
-                             └─ two.on('switch', ...)
-                             ┌─ two.send('isFocused')
-                             │
-   * window two - index.html ┘
-         oak.on('isFocused')
-
+one.html
+│
+└ `oak.send('switch')`
+   │
+   └─> src/index.js
+        │
+        └ `two.on('switch', ...)`
+        │
+        └ `two.send('isFocused')`
+        │
+        └─> src/two.html
+             │
+             └ `oak.on('isFocused')`
 ```
--- examples/multiple-windows/src/index.js
+++ examples/multiple-windows/src/index.js
@@ -1,68 +1,26 @@
 const { join } = require('path')
 const oak = require('oak')
 
-const Hapi = require('hapi')
-const inert = require('inert')
-
-// load hapi, serve static files
-const server = new Hapi.Server({
-  connections: {
-    routes: {
-      files: {
-        relativeTo: join(__dirname)
-      }
-    }
-  }
-})
-
-server.connection({
-  port: process.env.PORT || 9999
-})
-
-server.register(inert, () => {})
-
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: '.',
-      redirectToSlash: true,
-      index: true
-    }
-  }
-})
-
-// start hapi
-server.start((err) => {
-  if (err) {
-    throw err
-  }
-})
-oak.catchErrors()
 // main app
 oak.on('ready', () => {
   let one = oak.load({
-    url: 'http://localhost:9999/index.html',
-    scripts: ['lodash'],
+    url: 'file://' + join(__dirname, 'one.html'),
     fullscreen: false,
     ontop: false
   })
 
-  one.on('switch', function () {
-      // the event has been fired by this windows index.html via `oak.send()`
-      two.focus().send('isFocused')
-    })
-
   let two = oak.load({
-    url: 'http://localhost:9999/index.html',
-    scripts: ['lodash'],
+    url: 'file://' + join(__dirname, 'two.html'),
     fullscreen: false,
     ontop: false
+  })
+
+  // the event has been fired by this windows html via `oak.send()`
+  one.on('switch', function () {
+    two.focus().send('isFocused')
   })
 
   two.on('switch', function () {
-    // the event has been fired by this windows index.html via `oak.send()`
     one.focus().send('isFocused')
   })
 })
